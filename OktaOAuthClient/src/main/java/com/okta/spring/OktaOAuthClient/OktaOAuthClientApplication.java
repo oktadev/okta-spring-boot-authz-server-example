d8ca09821537bd0ba35d9f576bf6de16eb26c879
Matt Raible <matt.raible@okta.com>
2019-02-28 23:33:30 -0700

    Checkin of completed project

---
+++ OktaOAuthClient/src/main/java/com/okta/spring/OktaOAuthClient/OktaOAuthClientApplication.java
@@ -0,0 +1,13 @@
+package com.okta.spring.OktaOAuthClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaOAuthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaOAuthClientApplication.class, args);
+	}
+
+}
---
+++ OktaOAuthClient/src/main/java/com/okta/spring/OktaOAuthClient/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.okta.spring.OktaOAuthClient;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+            .authorizeRequests()
+            .antMatchers("/")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and().oauth2Login();
+    }
+}
