e3e5d06b8a75ee0c49f0d6df82d1219938fc1617
Matt Raible <matt.raible@okta.com>
2019-03-01 12:26:20 -0700

    Remove unnecessary parent class

--- AuthorizationServerApplication/src/main/java/com/okta/spring/AuthorizationServerApplication/AuthorizationServerApplication.java
+++ AuthorizationServerApplication/src/main/java/com/okta/spring/AuthorizationServerApplication/AuthorizationServerApplication.java
@@ -2,12 +2,11 @@ package com.okta.spring.AuthorizationServerApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
-public class AuthorizationServerApplication extends SpringBootServletInitializer {
+public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationServerApplication.class, args);
